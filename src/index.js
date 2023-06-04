4f2fd2f9b7201ef24d91300ea2e106ee2f640e9f
Alex <112801683+Alexey-Zaharov@users.noreply.github.com>
2023-06-04 15:02:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/tweet-card">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
